646c7cb7e0f67c0940084c02926448844c4fba66
imransupp <81986553+imransupp@users.noreply.github.com>
2024-08-16 07:24:54 -0500

    Update TestController.java

--- src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
 	public Object hello() {
 		Map<String, String> object = new HashMap<>();
 		object.put("name", "Integration Test");
-		object.put("email", "imransupp@gmail");
+		object.put("email1", "imransupp@gmail");
 		return object;
 	}
 
